a489d223b56f1d65a6403ad79780c144ed7d18e0
Kido <kalifatullah86@gmail.com>
2014-05-12 15:39:06 +0700

    1. Menambahkan css & jquery 2. Memodifikasi tampilan auth/login.php

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,18 +1,25 @@
+<!doctype html>
+<html lang = "en">
+<head>
+<link rel="stylesheet" href="<?php echo base_url();?>css/style.css" type="text/css">
+<script src ="<?php echo base_url();?>js/jquery-1.8.0.min.js" type="text/javascript"></script>
+<script src ="<?php echo base_url();?>js/home.js" type="text/javascript"></script>
+</head>
+<body>
+	<div id = "login_form">
 <h1><?php echo lang('login_heading');?></h1>
-<p><?php echo lang('login_subheading');?></p>
+
 
 <div id="infoMessage"><?php echo $message;?></div>
 
 <?php echo form_open("auth/login");?>
 
   <p>
-    <?php echo lang('login_identity_label', 'identity');?>
-    <?php echo form_input($identity);?>
+    <?php echo form_input($identity, '',  'placeholder="Your Email"');?>
   </p>
 
   <p>
-    <?php echo lang('login_password_label', 'password');?>
-    <?php echo form_input($password);?>
+    <?php echo form_input($password, '',  'placeholder="Your Password"');?>
   </p>
 
   <p>
@@ -21,8 +28,10 @@
   </p>
 
 
-  <p><?php echo form_submit('submit', lang('login_submit_btn'));?></p>
+  <?php echo form_submit('submit', lang('login_submit_btn'));?>
 
 <?php echo form_close();?>
 
-<p><a href="forgot_password"><?php echo lang('login_forgot_password');?></a></p>
+<a href="forgot_password"><?php echo lang('login_forgot_password');?></a>
+</div>
+</body></html>
